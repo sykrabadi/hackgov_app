acd696c3069c8550051bb7b73f155bae4d499520
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-06-20 23:56:03 +0700

    Additional changes on app.py and database
    Add pembangunanSektorTable on app.py, add few records on database, and adjustments with templates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proyek.db'
@@ -12,22 +13,36 @@ css = Bundle("src/main.css", output="dist/main.css", filters="postcss")
 assets.register("css", css)
 css.build()
 
-class Sektor_Table(db.Model):
+class sektorTable(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     alias_sektor = db.Column(db.String(length=1024), nullable=False)
     nama_sektor = db.Column(db.String(length=1024), nullable=False)
+    pembangunan = db.relationship('pembangunanSektorTable', backref='relasi_antarsektor')
 
     def __repr__(self):
-        return f'{self.alias_sektor}'
+        return f'{self.id}'
+
+class pembangunanSektorTable(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    kodeTender = db.Column(db.Integer())
+    nilaiHps = db.Column(db.Integer())
+    namaPembangunan = db.Column(db.String(length=2048), nullable=False)
+    statusPembangunan = db.Column(db.String(length=1024), nullable=False)
+    kualifikasiUsaha = db.Column(db.String(length=1024), nullable=False)
+    syaratKualifikasi = db.Column(db.String(length=4096), nullable=False)
+    relasi_sektor = db.Column(db.Integer(), db.ForeignKey('sektor_table.id'))
+
+    def __repr__(self):
+         return f'{self.kodeTender}'
 
 @app.route("/home")
 def home():
-    sektor = Sektor_Table.query.all()
+    sektor = sektorTable.query.all()
     return render_template("index.html", sektor = sektor)
 
 @app.route("/sektor/<sektor>")
 def sektor(sektor):
-    sektor = Sektor_Table.query.filter_by(alias_sektor = sektor).first()
+    sektor = sektorTable.query.filter_by(id = sektor).first()
     return render_template("sektor.html", sektor = sektor)
 
 if __name__ == "__main__":
